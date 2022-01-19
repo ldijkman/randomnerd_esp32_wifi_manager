5a078607c7d492f9bf2f1c9d897149dc4af4264c
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-01-19 05:00:10 +0100

    Update ESP32_WiFi_Manager.ino

--- ESP32_WiFi_Manager.ino
+++ ESP32_WiFi_Manager.ino
@@ -659,7 +659,15 @@ void browseService(const char * service, const char * proto) {
   
   Serial.print("WiFi.status == ");
   Serial.println(WiFi.status());
-
+  /*
+    WL_IDLE_STATUS      = 0,
+    WL_NO_SSID_AVAIL    = 1,
+    WL_SCAN_COMPLETED   = 2,
+    WL_CONNECTED        = 3,
+    WL_CONNECT_FAILED   = 4,
+    WL_CONNECTION_LOST  = 5,
+    WL_DISCONNECTED     = 6
+*/
 
 
   // Got it working???
