e9d474f4ad54110665cd26db6bb6c10ef8d30fff
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-02-13 09:49:02 +0100

    Add files via upload

---
+++ ESP8266-LittleFS-wifi-manager/ESP8266-LittleFS-wifi-manager_with_editor/data/index.js
@@ -0,0 +1,62 @@
+/**
+ * ----------------------------------------------------------------------------
+ * ESP32 Remote Control with WebSocket
+ * ----------------------------------------------------------------------------
+ * © 2020 Stéphane Calderoni
+ * ----------------------------------------------------------------------------
+ */
+
+var gateway = `ws://${window.location.hostname}/ws`;
+var websocket;
+
+// ----------------------------------------------------------------------------
+// Initialization
+// ----------------------------------------------------------------------------
+
+window.addEventListener('load', onLoad);
+
+function onLoad(event) {
+    initWebSocket();
+    initButton();
+}
+
+// ----------------------------------------------------------------------------
+// WebSocket handling
+// ----------------------------------------------------------------------------
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage;
+}
+
+function onOpen(event) {
+    console.log('Connection opened');
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+function onMessage(event) {
+    let data = JSON.parse(event.data);
+    document.getElementById('ledstate').innerHTML = data.status;
+    document.getElementById('image').src = data.status+".gif";
+    document.getElementById('mdnsscan').innerHTML = data.scan;
+    document.getElementById('NTPTime').innerHTML = data.time;
+}
+
+// ----------------------------------------------------------------------------
+// Button handling
+// ----------------------------------------------------------------------------
+
+function initButton() {
+    document.getElementById('toggle').addEventListener('click', onToggle);
+}
+
+function onToggle(event) {
+    websocket.send(JSON.stringify({'action':'toggle'}));
+}
