2869a12c52afe476d9f4efdfc65575f6f8add2d7
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-06-05 08:20:30 +0200

    Update ESP_AsyncFSBrowser.ino

--- docs/Touch_Electra-Electra_Touch/ESP32/ESP_AsyncFSBrowser/ESP_AsyncFSBrowser.ino
+++ docs/Touch_Electra-Electra_Touch/ESP32/ESP_AsyncFSBrowser/ESP_AsyncFSBrowser.ino
@@ -1,4 +1,4 @@
-// hell to get it running on ESP32
+// HELL to get it running on ESP32
 // some screenshots off my arduino ide settings and versions
 // https://github.com/ldijkman/randomnerd_esp32_wifi_manager/tree/main/docs/Touch_Electra-Electra_Touch/ESP32
 
