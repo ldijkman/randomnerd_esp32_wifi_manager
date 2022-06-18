fe4d2a24b9de575665c720c3df062730d1bfe49e
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-06-18 05:11:05 +0200

    Update User_Setup.h

--- docs/Touch_Electra-Electra_Touch/ESP32/ESP32_Electra/User_Setup.h
+++ docs/Touch_Electra-Electra_Touch/ESP32/ESP32_Electra/User_Setup.h
@@ -1,10 +1,6 @@
- // https://oshwlab.com/l.dijkman/esp32-dev-kit-38-pin-to-spi-touch-tft 
-// https://github.com/ldijkman/ART-ESP32-Touch-TFT-Thermostat 
-// http://www.Arduino.TK
-
-// https://www.facebook.com/groups/esp32smartthermostat
+// https://ldijkman.github.io/Electra/
    
-// Shortened TFT_eSPI Graphic_Library User_Setup.h to match Art's Hardware ESP32 38Pin SPI Touch TFT Settings    
+// TFT_eSPI User_Setup.h for Electra ESP32   
     
 // This  filelocation => /home/pi/Arduino/libraries/TFT_eSPI/User_Setup.h
 // windows user_setup.h file location = I do not now  programfiles/arduino....  or documents/arduino.....
@@ -14,23 +10,22 @@
 // define only one screen driver, the other ones must be commented out
 //%%%%%%%%%%%%%%%%
 // #define ILI9341_DRIVER     // My, Luberth tested ok, 2.8 inch & 3.2 inch SPI TFT display 320x240
-// #define ILI9488_DRIVER     // My, Luberth tested ok, 3.5 inch SPI TFT display 480x320 will work, Future Big Art
-#define ST7796_DRIVER         // My, Luberth tested ok, 4 inch SPI TFT display 480x320 will work, Future Big Art
+ #define ILI9488_DRIVER     // My, Luberth tested ok, 3.5 inch SPI TFT display 480x320 will work, Future Big Art
+//#define ST7796_DRIVER         // My, Luberth tested ok, 4 inch SPI TFT display 480x320 will work, Future Big Art
 
 // SPI section http://www.lcdwiki.com/Main_Page // has more info on SPI Displays
 
 // ###### EDIT THE PIN NUMBERS IN THE LINES FOLLOWING TO SUIT YOUR ESP32 SETUP   ######
 
 //TFT LED could be +3.3Vdc or gpio pin?? for PWM backlight brightness control, see my code for used GpioPinName
-#define TFT_MISO 19
+//#define TFT_MISO 19    miso not used
 #define TFT_MOSI 23
 #define TFT_SCLK 18
 #define TFT_CS   15  
 #define TFT_DC    2 
-#define TFT_RST   4 
-/////////////////// #define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
+#define TFT_RST   -1      // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
-#define TOUCH_CS 21     // Chip select pin (T_CS) of touch screen
+#define TOUCH_CS 25     // Chip select pin (T_CS) of touch screen
 
 #define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
 #define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
@@ -49,4 +44,8 @@
 
 #define SPI_TOUCH_FREQUENCY  2000000
 
-#define USE_HSPI_PORT                  // 
+//#define USE_HSPI_PORT                  // touch does not work if this is on
+
+// https://ldijkman.github.io/Electra/
+
+
