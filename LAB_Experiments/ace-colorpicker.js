27bb4f86a96e2e96c1b8f0c6e11525a9217034d2
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2022-07-31 08:47:20 +0200

    Update ace-colorpicker.js

--- LAB_Experiments/ace-colorpicker.js
+++ LAB_Experiments/ace-colorpicker.js
@@ -1,3 +1,6 @@
+
+/*  https://www.jsdelivr.com/package/npm/ace-colorpicker */
+
 (function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
 var AceColorPicker = (function () {
 'use strict';
