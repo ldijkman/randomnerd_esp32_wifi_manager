8dce7f44ce300c0feeacd167844157a288438c9a
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2023-10-03 16:58:38 +0200

    Update ext-scrollbar.js

--- docs/mrs/Ace_color/ext-scrollbar.js
+++ docs/mrs/Ace_color/ext-scrollbar.js
@@ -4,9 +4,7 @@ https://github.com/ukandrewc/Ace-Scrollbars
 https://github.com/ukandrewc/Ace-Scrollbars/blob/master/src/ext-scrollbar.js
 
 changes made by luberth
-https://github.com/ldijkman/randomnerd_esp32_wifi_manager/blob/main/docs/mrs/Ace_color/ext-scrollbar.js
-https://plnkr.co/plunk/EYJWlWGjCdjNoui8
-https://plnkr.co/edit/Digj5Fw7D6hxaTU4
+https://ldijkman.github.io/Ace_Seventh_Heaven/Hell.html
 
 my editor does not start at mousepos/windowpos y zero
 so i treid to make an offset for that
@@ -17,9 +15,29 @@ treid to change from pixel positions to percent positions
 not perfect yet
 but works a bit better i think
 
+i am a mechanic, i screw things
+   made some changes to make it read better for me
+      not optimized for speed
+
 https://m.facebook.com/luberth.dijkman
 
 */
+
+function id(id) {
+  return document.getElementById(id);
+}
+
+function classname(classname) {
+  return document.getElementsByClassName(classname);
+}
+
+///////////////////////////////////////////////////////////////////////////
+// arduino alike map function == Re-maps a number from one range to another. 
+// https://www.arduino.cc/reference/en/language/functions/math/map/ 
+///////////////////////////////////////////////////////////////////////////////
+function map_range(value, low1, high1, low2, high2) {
+    return (low2 + (high2 - low2) * (value - low1) / (high1 - low1)).toFixed(3);   // to 3 decimals, Math.round did not scale right
+}
 
 function AceScrollbars(e) {
 
@@ -31,7 +49,7 @@ function AceScrollbars(e) {
 
 	// Ace scrollbars
 	let gWidth = 34
-	let sc = document.getElementsByClassName("ace_scrollbar")
+	let sc = classname("ace_scrollbar")
 
 	let ro = new MutationObserver(function () {
 		resizeScroll()
@@ -48,8 +66,6 @@ function AceScrollbars(e) {
 	}
 
 	// Map
-	let mr = document.getElementById("ace_map-row")
-	let mc = document.getElementById("ace_map")
 
 	let h = 0
 	e.on("changeSelection", function () {
@@ -57,17 +73,12 @@ function AceScrollbars(e) {
 		h = setTimeout(showMapItems, 250)
 	})
 
-///////////////////////////////////////////////////////////////////////////
-// arduino alike map function == Re-maps a number from one range to another. 
-// https://www.arduino.cc/reference/en/language/functions/math/map/ 
-// https://stackoverflow.com/questions/5649803/remap-or-map-function-in-javascript
-// scale range 1 into range2 bar range to 0 to 1440 minute range  
-// or 
-// scale a minutevalue in range 0 to 1440 minutes to 0% to 100%,  used for left and width of timeslot divs
-///////////////////////////////////////////////////////////////////////////////
-function map_range(value, low1, high1, low2, high2) {
-    return (low2 + (high2 - low2) * (value - low1) / (high1 - low1)).toFixed(3);   // to 3 decimals, Math.round did not scale right
-}
+
+
+
+
+
+
 
 
 	showMapItems = function () {
@@ -92,8 +103,8 @@ function map_range(value, low1, high1, low2, high2) {
 				}
 			}
 		}
-		mc.innerHTML = ht
-		mr.style.top = map_range(e.getCursorPosition().row,0,editor.session.getLength()+50, 0,100)+"%"
+		id("ace_map").innerHTML = ht
+		id("ace_map-row").style.top = map_range(e.getCursorPosition().row,0,editor.session.getLength()+50, 0,100)+"%"
 		//console.log("rScale2",rScale);
 	}
 
@@ -104,11 +115,11 @@ function map_range(value, low1, high1, low2, high2) {
 	let vStart = -1
 
 	let vs = sc[0]
-	let tv = document.getElementById("ace_bar-v")    // tv is the gray view on scrollbar, should match the editor view position
+	let tv = id("ace_bar-v")    // tv is the gray view on scrollbar, should match the editor view position
 
 	vs.addEventListener("scroll", function (e) {//think it needes an offset here if editor does not start at top 0 of document
 	//	tv.style.top = (vs.scrollTop-document.getElementById("editor").offsetTop) * vScale + "px"
-		tv.style.top = (map_range(vs.scrollTop-document.getElementById("editor").offsetTop,0,vs.scrollHeight+vs.clientHeight,0,100))+"%"
+		tv.style.top = (map_range(vs.scrollTop-id("editor").offsetTop,0,vs.scrollHeight+vs.clientHeight,0,100))+"%"
 		//tv.style.top = (vs.scrollTop-e.offsetTop) * vScale + "px"
 
 	})
@@ -118,7 +129,7 @@ function map_range(value, low1, high1, low2, high2) {
 		tv.setPointerCapture(e.pointerId)
 		vStart = e.clientY
     		//console.log("e.clientY",e.clientY);
-    		//console.log("mc.offsetTop+'px'",mc.offsetTop);
+    		//console.log("id("ace_map").offsetTop+'px'",id("ace_map").offsetTop);
     		//console.log("offsetTop",document.getElementById("editor").offsetTop);
 				console.log();
     
@@ -138,7 +149,7 @@ function map_range(value, low1, high1, low2, high2) {
 
 	tv.previousElementSibling.addEventListener("mousedown", function (e) {
 		// my editor does not start at zero because of my non editor buttons menu so offset needed
-     		vs.scrollTop = (e.clientY-document.getElementById("editor").offsetTop) / vScale
+     		vs.scrollTop = (e.clientY-id("editor").offsetTop) / vScale
 	   	//vs.scrollTop = (e.clientY-e.offsetTop) / vScale
 
 	})
@@ -148,34 +159,8 @@ function map_range(value, low1, high1, low2, high2) {
 	let hStart = -1
 
 	let hs = sc[1]
-	let th = document.getElementById("ace_bar-h")
-/*
-	hs.addEventListener("scroll", function () {
-		th.style.left = (hs.scrollLeft * hScale) + gWidth + "px"
-	})
+	let th = id("ace_bar-h")
 
-	// Drag events
-	th.addEventListener("pointerdown", function (e) {
-		th.setPointerCapture(e.pointerId)
-		hStart = e.clientX
-	})
-
-	th.addEventListener("pointermove", function (e) {
-		if (hStart != -1) {
-			hs.scrollLeft += (e.clientX - hStart) / hScale
-			hStart = e.clientX
-		}
-	})
-
-	th.addEventListener("pointerup", function (e) {
-		th.releasePointerCapture(e.pointerId)
-		hStart = -1
-	})
-
-	th.previousElementSibling.addEventListener("mousedown", function (e) {
-		hs.scrollLeft = e.clientX / hScale
-	})
-*/
 	resizeScroll = function () {
 		vScale = vs.clientHeight / vs.scrollHeight
 		if (vs.clientHeight) {
@@ -185,17 +170,7 @@ function map_range(value, low1, high1, low2, high2) {
 		else {
 			showScroll(tv, false)
 		}
-/*
-		hScale = hs.clientWidth / hs.scrollWidth
-		if (hs.clientWidth) {
-			th.style.width = (hs.clientWidth * hScale) + "px"
-			th.style.left = (hs.scrollLeft * hScale) + gWidth + "px"
-			showScroll(th, true)
-		}
-		else {
-			showScroll(th, false)
-		}
-*/
+
 		rScale = e.renderer.lineHeight * vScale
 		//console.log("e.renderer.lineHeight",e.renderer.lineHeight);
 		gWidth = e.renderer.gutterWidth
